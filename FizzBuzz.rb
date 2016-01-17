64d15a4807588a6b583ddcb6566419e5f123b953
rinykia <rinykia@gmail.com>
2016-01-17 20:38:07 +0200

    6. FizzBuzz on ruby

--- FizzBuzz.rb
+++ FizzBuzz.rb
@@ -1,9 +1,9 @@
 1.upto(100) do |i|
-  if  i%3 == 0 && i%5 == 0
+  if  (i%3 == 0 && i%5 == 0)
     puts "FizzBuzz"
-  elsif i%5 == 0
+  elsif (i%5 == 0)
     puts "Buzz"
-  elsif i%3 == 0
+  elsif (i%3 == 0)
     puts "Fizz"
   else
     puts i
